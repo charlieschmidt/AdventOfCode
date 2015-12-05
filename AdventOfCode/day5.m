c8cf88373c3d623f1e2cce914d95ed4f41ccde0f
Charlie Schmidt <cschmidt2@gmail.com>
2015-12-05 09:12:53 -0500

    day 5 redone with regular expressions

--- AdventOfCode/day5.m
+++ AdventOfCode/day5.m
@@ -6,39 +6,24 @@
     
     int problemNum = 2;
     
+    
+    NSError *error = nil;
+    NSRegularExpression *vowelsRegex = [NSRegularExpression regularExpressionWithPattern:@"[aeiou]" options:0 error:&error];
+    NSRegularExpression *badStringsRegex = [NSRegularExpression regularExpressionWithPattern:@"ab|cd|pq|xy" options:0 error:&error];
+    NSRegularExpression *doubleLettersRegex = [NSRegularExpression regularExpressionWithPattern:@"([a-z])\\1" options:0 error:&error];
+    NSRegularExpression *twoPairsRegex = [NSRegularExpression regularExpressionWithPattern:@"([a-z][a-z])[a-z]*\\1" options:0 error:&error];
+    NSRegularExpression *xyxRegex = [NSRegularExpression regularExpressionWithPattern:@"([a-z])[a-z]\\1" options:0 error:&error];
+
+    
     for (NSString *input in inputs)
     {
         printf("Input: %s\n",[input UTF8String]);
         
         if (problemNum == 1)
         {
-            NSInteger vowelCount = 0;
-            char prevLetter = '\0';
-            BOOL hasDoubleLetters = NO;
-            BOOL hasBadStrings = NO;
-            
-            for (int i = 0; i < [input length]; i++)
-            {
-                char c = [input characterAtIndex:i];
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-                {
-                    vowelCount++;
-                }
-                
-                if (c == prevLetter)
-                {
-                    hasDoubleLetters = YES;
-                }
-                prevLetter = c;
-            }
-            
-            if ([input containsString:@"ab"] ||
-                [input containsString:@"cd"] ||
-                [input containsString:@"pq"] ||
-                [input containsString:@"xy"])
-            {
-                hasBadStrings = YES;
-            }
+            NSUInteger vowelCount = [vowelsRegex numberOfMatchesInString:input options:0 range:NSMakeRange(0,[input length])];
+            BOOL hasDoubleLetters = ([doubleLettersRegex numberOfMatchesInString:input options:0 range:NSMakeRange(0,[input length])] == 0 ? NO : YES);
+            BOOL hasBadStrings = ([badStringsRegex numberOfMatchesInString:input options:0 range:NSMakeRange(0,[input length])] == 0 ? NO : YES);
             
             if (vowelCount >= 3 && hasDoubleLetters == YES && hasBadStrings == NO)
             {
@@ -53,26 +38,8 @@
         }
         else
         {
-            BOOL hasTwoPairs = NO;
-            BOOL hasOneSeparatedByAnother = NO;
-            
-            for (int i = 0; i < [input length] - 2; i++)
-            {
-                NSString *pair = [input substringWithRange:NSMakeRange(i,2)];
-                
-                NSRange range = [input rangeOfString:pair options:NSLiteralSearch range:NSMakeRange(i+2,[input length] - (i+2))];
-                
-                if (range.location != NSNotFound)
-                {
-                    hasTwoPairs = YES;
-                }
-                
-                if ([input characterAtIndex:i] == [input characterAtIndex:i+2])
-                {
-                    hasOneSeparatedByAnother = YES;
-                }
-                
-            }
+            BOOL hasTwoPairs = ([twoPairsRegex numberOfMatchesInString:input options:0 range:NSMakeRange(0,[input length])] == 0 ? NO : YES);
+            BOOL hasOneSeparatedByAnother = ([xyxRegex numberOfMatchesInString:input options:0 range:NSMakeRange(0,[input length])] == 0 ? NO : YES);
             
             if (hasOneSeparatedByAnother == YES && hasTwoPairs == YES)
             {
