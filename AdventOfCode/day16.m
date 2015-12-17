9c831634c2e9fc552a2427d3d985c0bd60b92f14
Charlie Schmidt <cschmidt2@gmail.com>
2015-12-17 08:17:34 -0500

    day 16 and day 17

---
+++ AdventOfCode/day16.m
@@ -0,0 +1,98 @@
+
+- (void)day16:(NSArray *)inputs part:(NSNumber *)part
+{
+    NSDictionary *giftInformation = @{
+                                      @"children": @3,
+                                      @"cats": @7,
+                                      @"samoyeds": @2,
+                                      @"pomeranians": @3,
+                                      @"akitas": @0,
+                                      @"vizslas": @0,
+                                      @"goldfish": @5,
+                                      @"trees": @3,
+                                      @"cars": @2,
+                                      @"perfumes": @1
+                                      };
+    NSMutableArray *sueInformations = [[NSMutableArray alloc] init];
+    NSError *error = nil;
+    
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"Sue (\\d*): (\\w*): (\\d*), (\\w*): (\\d*), (\\w*): (\\d*)" options:0 error:&error];
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    f.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    for (NSString *input in inputs)
+    {
+        NSArray *matches = [regex matchesInString:input options:0 range:NSMakeRange(0,[input length])];
+        for (NSTextCheckingResult *result in matches)
+        {
+            NSNumber *sueNumber = [f numberFromString:[input substringWithRange:[result rangeAtIndex:1]]];
+            NSString *thing1 = [input substringWithRange:[result rangeAtIndex:2]];
+            NSNumber *countOfThing1 = [f numberFromString:[input substringWithRange:[result rangeAtIndex:3]]];
+            NSString *thing2 = [input substringWithRange:[result rangeAtIndex:4]];
+            NSNumber *countOfThing2 = [f numberFromString:[input substringWithRange:[result rangeAtIndex:5]]];
+            NSString *thing3 = [input substringWithRange:[result rangeAtIndex:6]];
+            NSNumber *countOfThing3 = [f numberFromString:[input substringWithRange:[result rangeAtIndex:7]]];
+            
+            NSMutableDictionary *information = [[NSMutableDictionary alloc] init];
+            [information setObject:sueNumber forKey:@"sueNumber"];
+            [information setObject:countOfThing1 forKey:thing1];
+            [information setObject:countOfThing2 forKey:thing2];
+            [information setObject:countOfThing3 forKey:thing3];
+            [sueInformations addObject:information];
+            
+        }
+    }
+    
+    BOOL fuzzy = ([part intValue] == 2);
+    
+    for (int i = 0; i < [sueInformations count]; i++)
+    {
+        BOOL isThisOne = YES;
+        NSMutableDictionary *information = [sueInformations objectAtIndex:i];
+    
+        for (NSString *key in [information allKeys])
+        {
+            if ([key isEqualToString:@"sueNumber"])
+            {
+                continue;
+            }
+            NSNumber *informationValue = [information objectForKey:key];
+            NSNumber *giftValue = [giftInformation objectForKey:key];
+            BOOL b = [self compareInformation:key giftValue:giftValue sueValue:informationValue fuzzy:fuzzy];
+
+            if (b == NO)
+            {
+                isThisOne = NO;
+                break;
+            }
+        }
+        
+        if (isThisOne)
+        {
+            NSLog(@"Part %@: Sue %@\n",part, [information objectForKey:@"sueNumber"]);
+            break;
+        }
+    }
+}
+
+- (BOOL) compareInformation:(NSString *)key giftValue:(NSNumber *)giftValue sueValue:(NSNumber *)sueValue fuzzy:(BOOL)fuzzy
+{
+    if (fuzzy == NO)
+    {
+        return [giftValue isEqualToNumber:sueValue];
+    }
+    else
+    {
+        if ([key isEqualToString:@"cats"] || [key isEqualToString:@"trees"])
+        {
+            return ([giftValue intValue] < [sueValue intValue]);
+        }
+        
+        if ([key isEqualToString:@"pomeranians"] || [key isEqualToString:@"goldfish"])
+        {
+            return ([giftValue intValue] > [sueValue intValue]);
+        }
+        
+        return [giftValue isEqualToNumber:sueValue];
+    }
+}
---
+++ AdventOfCode/day17.m
@@ -0,0 +1,81 @@
+
+- (void)day17:(NSArray *)inputs
+{
+    int volumeGoal = 150;
+    NSMutableArray *containers = [[NSMutableArray alloc] init];
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    f.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    for (NSString *input in inputs)
+    {
+        NSNumber *number = [f numberFromString:input];
+        [containers addObject:number];
+    
+    }
+    
+    
+    uint64_t num_combos = 1ull << [containers count];    // 2**count
+    NSMutableArray *combos = [NSMutableArray new];
+    for (uint64_t mask = 1; mask < num_combos; mask++) {
+        NSMutableIndexSet *indexes = [NSMutableIndexSet indexSet];
+        
+        for (uint64_t i = 0; i < 64; i++) {
+            if (mask & (1ull << i) ){
+                [indexes addIndex:i];
+            }
+        }
+        [combos addObject:[containers objectsAtIndexes:indexes]];
+    }
+    
+    int containersThatWork = 0;
+    int minimumCount = [containers count];
+    
+    for (int i = 0; i < [combos count]; i++)
+    {
+        NSArray *subContainers = [combos objectAtIndex:i];
+        int volume = 0;
+        
+        for (int j = 0; j < [subContainers count]; j++)
+        {
+            NSNumber *number = [subContainers objectAtIndex:j];
+            
+            volume += [number intValue];
+        }
+        
+        if (volume == volumeGoal)
+        {
+            containersThatWork++;
+            
+            if ([subContainers count] < minimumCount)
+            {
+                minimumCount = [subContainers count];
+            }
+        }
+        
+    }
+    
+    int combosWithMinimumCount = 0;
+    
+    for (int i = 0; i < [combos count]; i++)
+    {
+        NSArray *subContainers = [combos objectAtIndex:i];
+        int volume = 0;
+        
+        for (int j = 0; j < [subContainers count]; j++)
+        {
+            NSNumber *number = [subContainers objectAtIndex:j];
+            
+            volume += [number intValue];
+        }
+        
+        if (volume == volumeGoal && [subContainers count] == minimumCount)
+        {
+            combosWithMinimumCount++;
+        }
+    }
+    
+    
+    NSLog(@"%d\n",containersThatWork);
+    
+    NSLog(@"%d with minimum count %d\n",combosWithMinimumCount, minimumCount);
+}
